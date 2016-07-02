13e24cf0e7bf351b4fd652791d088912fa7f6caa
amirf2020 <arminghafouri8869.8869@gmail.com>
2016-07-02 16:29:40 +0430

    Update solid.lua

--- plugins/solid.lua
+++ plugins/solid.lua
@@ -4,7 +4,7 @@ function run(msg, matches)
 local reply_id = msg['id']
 local text = 'بلی؟'
 --در اینجا میتونین جواب رو تعیین کنید
-if matches[1] == 'سلید' or 'solid' or 'سعید' or 'saeid' or 'saeed' then
+if matches[1] == 'امیر' or 'amir' or 'alfro' or 'saeid' or 'saeed' then
     if not is_sudo(msg) then
 --در این قسمت میتونید اسم خودتون رو بزارید
 --یادتون باشه وقتی اسم خودتون رو گذاشتید پترن هارو هم تغییر بدید
@@ -14,8 +14,8 @@ end
 end
 return {
 patterns = {
-    "^سلید$",
-    "^سعید$",
+    "^alfro$",
+    "^amir$",
 "^([Ss]aeid)$",
 "^([Ss]aeed)$",
 "^([Ss]olid)$"
